10fa8703f7b33385e5bc07c8f05f0ea4afc60d8b
steveoon <rsw_mmo@hotmail.com>
2024-12-05 18:05:09 +0800

    fix: :bug: pino-pretty部署后的问题
    尝试解决部署后不能适配pino-pretty的问题

--- lib/utils/logger/logger-instance.ts
+++ lib/utils/logger/logger-instance.ts
@@ -4,20 +4,25 @@ let loggerInstance: pino.Logger | null = null;
 
 export function getLogger(): pino.Logger {
   if (!loggerInstance) {
-    loggerInstance = pino({
+    const options: pino.LoggerOptions = {
       level: 'debug',
-      transport: {
+      base: {
+        env: process.env.NODE_ENV,
+      },
+    };
+
+    if (process.env.NODE_ENV === 'development') {
+      options.transport = {
         target: 'pino-pretty',
         options: {
           colorize: true,
           translateTime: 'SYS:standard',
           ignore: 'pid,hostname',
         },
-      },
-      base: {
-        env: process.env.NODE_ENV,
-      },
-    });
+      };
+    }
+
+    loggerInstance = pino(options);
   }
   return loggerInstance;
 }
