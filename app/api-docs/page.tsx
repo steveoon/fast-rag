fb11233323184d0a98ddc11b88637d710bd03ab3
steveoon <rsw_mmo@hotmail.com>
2025-04-02 00:48:42 +0800

    fix: :bug: 提示API文档正在建设
    RT

--- app/api-docs/page.tsx
+++ app/api-docs/page.tsx
@@ -2,7 +2,9 @@ export default function ApiDocsPage() {
   return (
     <section className="container mx-auto pt-8 pb-16">
       <h1 className="text-2xl font-bold mb-6 px-4">Wolian AI API</h1>
-      <div className="bg-white py-4 px-2 rounded-lg shadow">It is Comming Soon</div>
+      <div className="bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100 py-4 px-2 rounded-lg shadow">
+        <div className="text-center text-2xl font-bold">Comming Soon</div>
+      </div>
     </section>
   );
 }
