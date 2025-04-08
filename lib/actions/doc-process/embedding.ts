8791f877628e35ad5e0f7e93c541acfb3d6de759
steveoon <rsw_mmo@hotmail.com>
2025-04-08 22:35:25 +0800

    fix: :bug: 修改模型ID格式
    RT

--- lib/actions/doc-process/embedding.ts
+++ lib/actions/doc-process/embedding.ts
@@ -3,7 +3,7 @@ import { registry } from '@/lib/utils/models-registry';
 
 export async function embedding(values: string[]) {
   const { embeddings } = await embedMany({
-    model: registry.textEmbeddingModel('openai:text-embedding-3-small'),
+    model: registry.textEmbeddingModel('openai/text-embedding-3-small'),
     values,
   });
   return embeddings;
