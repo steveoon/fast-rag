8d9dd06932c67918c455ca7f45e05c02fbe3ee19
steveoon <rsw_mmo@hotmail.com>
2025-04-09 15:43:25 +0800

    fix: :bug: 旧版本ai sdk还创建language registry还没移除experimental_前缀
    RT

--- lib/utils/models-registry/index.ts
+++ lib/utils/models-registry/index.ts
@@ -1,7 +1,7 @@
 import { createAnthropic } from '@ai-sdk/anthropic';
 import { createOpenAI } from '@ai-sdk/openai';
 import { createGoogleGenerativeAI } from '@ai-sdk/google';
-import { createProviderRegistry } from 'ai';
+import { experimental_createProviderRegistry as createProviderRegistry } from 'ai';
 
 export const registry = createProviderRegistry({
   // register provider with prefix and default setup:
